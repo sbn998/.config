4477d1d9cb3b4ab138093ece5312b9b47801ad4b
sbn998 <sergib1998@gmail.com>
2025-02-06 23:49:23 +0100

    removed indent-blankline in favour of snacks indent

---
+++ nvim/lua/config/snacks/indent.lua
@@ -0,0 +1,11 @@
+local M = {
+	enabled = true,
+	indent = {
+		char = "┊",
+	},
+	scope = {
+		char = "┊",
+	},
+}
+
+return M
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
 		dashboard = require("config.snacks.dashboard"),
 		explorer = require("config.snacks.explorer"),
 		picker = require("config.snacks.picker"),
+		indent = require("config.snacks.indent"),
 	},
 	keys = require("config.snacks.keymaps"),
 }
