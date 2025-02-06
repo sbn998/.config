8cc0d28703faeed95f59c788591efc2f60d8a308
sbn998 <sergib1998@gmail.com>
2025-02-06 19:08:00 +0100

    updated plugin url and removed unused decorations

--- nvim/lua/plugins/flutter.lua
+++ nvim/lua/plugins/flutter.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"akinsho/flutter-tools.nvim",
+		"nvim-flutter/flutter-tools.nvim",
 		lazy = false,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
@@ -8,17 +8,6 @@ return {
 		},
 		config = function()
 			require("flutter-tools").setup({
-				decorations = {
-					statusline = {
-						-- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
-						-- this will show the current version of the flutter app from the pubspec.yaml file
-						app_version = true,
-						-- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
-						-- this will show the currently running device if an application was started with a specific
-						-- device
-						device = true,
-					},
-				},
 				widget_guides = {
 					enabled = true,
 				},
