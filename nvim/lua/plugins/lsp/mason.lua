415315af886b91532fa8f6691a5ebee36494bc62
sbn998 <sergib1998@gmail.com>
2025-02-06 15:47:01 +0100

    commented unused formatter and linter

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -31,12 +31,12 @@ return {
 		})
 		mason_tool_installer.setup({
 			ensure_installed = {
-				"prettier", -- prettier formatter
+				-- "prettier", -- prettier formatter
 				"stylua", -- lua formatter
 				-- "isort", -- python formatter
 				-- "black", -- python formatter
 				-- "pylint", -- python linter
-				"eslint_d", -- js linter
+				-- "eslint_d", -- js linter
 			},
 		})
 	end,
