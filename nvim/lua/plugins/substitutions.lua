c9f0ad7303ddd589448f24af759c0a2e9d8112a2
sbn998 <sergib1998@gmail.com>
2025-02-06 22:38:13 +0100

    added exchange and range substitution keymaps

--- nvim/lua/plugins/substitutions.lua
+++ nvim/lua/plugins/substitutions.lua
@@ -1,17 +1,24 @@
 return {
-  "gbprod/substitute.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local substitute = require("substitute")
+	"gbprod/substitute.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local substitute = require("substitute")
 
-    substitute.setup()
+		substitute.setup()
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-    vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-    vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-    vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-    vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
-  end,
+		keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
+		keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
+		keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+		keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+		keymap.set("n", "<leader>sr", require("substitute.range").operator, { noremap = true })
+		keymap.set("x", "<leader>sr", require("substitute.range").visual, { noremap = true })
+		keymap.set("n", "<leader>ss", require("substitute.range").word, { noremap = true })
+		keymap.set("n", "sr", require("substitute.exchange").operator, { noremap = true })
+		keymap.set("n", "srr", require("substitute.exchange").line, { noremap = true })
+		keymap.set("x", "X", require("substitute.exchange").visual, { noremap = true })
+		keymap.set("n", "src", require("substitute.exchange").cancel, { noremap = true })
+	end,
 }
