3eacf4d7b40ae4c3beab1f22c90ab674a2d581d6
sbn998 <sergib1998@gmail.com>
2025-02-07 00:22:14 +0100

    missing input table for keymaps

--- nvim/lua/config/snacks/picker.lua
+++ nvim/lua/config/snacks/picker.lua
@@ -1,14 +1,18 @@
 local M = {
 	win = {
-		keys = {
-			["<C-x>"] = { "close", mode = { "n", "i" } },
-			["<c-h>"] = { "edit_split", mode = { "i", "n" } },
+		input = {
+			keys = {
+				["<C-x>"] = { "close", mode = { "n", "i" } },
+				["<c-h>"] = { "edit_split", mode = { "i", "n" } },
+			},
 		},
 	},
 	lists = {
-		keys = {
-			["<C-x>"] = { "close", mode = { "n", "i" } },
-			["<c-h>"] = { "edit_split", mode = { "i", "n" } },
+		input = {
+			keys = {
+				["<C-x>"] = { "close", mode = { "n", "i" } },
+				["<c-h>"] = { "edit_split", mode = { "i", "n" } },
+			},
 		},
 	},
 }
